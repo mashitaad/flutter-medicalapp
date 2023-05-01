565a1198a39c214f22f2b21a785195481d667b86
Mashita Dewi <mashitaad@gmail.com>
2023-05-01 08:42:19 +0700

    feat: add bottom navbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,8 +10,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 24,
+        items: const[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.calendar_month_outlined,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.chat_bubble_outline,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications_none_outlined,
+              color: Colors.black54,
+            ),
+            label: '',
+          ),
+        ],
+      ),
     );
   }
 }
